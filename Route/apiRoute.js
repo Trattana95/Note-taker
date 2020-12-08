80cf5cad33aae481bea8967c1ff9aa10befab1dd
Thom Rattana <thomr95@gmail.com>
2020-12-08 14:28:44 -0500

    fixed apiRoute

--- Route/apiRoute.js
+++ Route/apiRoute.js
@@ -1,49 +1,31 @@
-var savedNotes = require("../db/db");
+var savedNotes = require("../db/db.json");
 const fs = require('fs');
 
+module.exports = function (app) {
 
-module.exports = function(app) {
-
-
-
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     res.json(savedNotes);
   });
 
-
-
-
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function (req, res) {
     var newNote = req.body;
-
     newNote.id = savedNotes.length.toString();
-    
+
     savedNotes.push(newNote);
-
-    fs.writeFile("./db/db.json", JSON.stringify(savedNotes),function(err) {
-            if (err) 
-            console.log (err);        
-        }); 
-
-        res.json(data);    
-
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function (err) {
+      if (err) throw(err);
     });
-
-    
-
-  //Deleting
-  app.delete("/api/notes/:id", function(req, res) {
+    res.json(data);
+  });
+  app.delete("/api/notes/:id", function (req, res) {
     var id = req.params.id;
-
     var atID = (element) => element.id === id;
     var deleteNote = savedNotes.findIndex(atID);
-
     savedNotes.splice(deleteNote, 1);
-    
     fs.writeFileSync("./db/db.json", JSON.stringify(data));
-        res.json(savedNotes);
+    res.json(savedNotes);
 
-    });
-    
-  };
+  });
+
+};
 
