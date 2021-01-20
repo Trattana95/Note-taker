baa70a55f5e599ad7181d346b053bc770dfde7b3
Thom Rattana <thomr95@gmail.com>
2021-01-19 23:43:56 -0500

    function works

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -64,15 +64,12 @@ const handleNoteSave = function () {
 
 // Delete the clicked note
 const handleNoteDelete = function (event) {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
+// prevents the click listener for the list from being called when the button inside of it is clicked
   event.stopPropagation();
-
   const note = $(this).parent(".list-group-item").data();
-
   if (activeNote.id === note.id) {
     activeNote = {};
   }
-
   deleteNote(note.id).then(() => {
     getAndRenderNotes();
     renderActiveNote();
@@ -113,7 +110,6 @@ const renderNoteList = (notes) => {
     const $li = $("<li class='list-group-item'>");
     const $span = $("<span>").text(text);
     $li.append($span);
-
     if (withDeleteButton) {
       const $delBtn = $(
         "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
@@ -121,17 +117,15 @@ const renderNoteList = (notes) => {
       $li.append($delBtn);
     }
     return $li;
-  };
-
+  }; 
+  
   if (notes.length === 0) {
     noteListItems.push(create$li("No saved Notes", false));
   }
-
   notes.forEach((note) => {
     const $li = create$li(note.title).data(note);
     noteListItems.push($li);
   });
-
   $noteList.append(noteListItems);
 };
 
@@ -139,6 +133,7 @@ const renderNoteList = (notes) => {
 const getAndRenderNotes = () => {
   return getNotes().then(renderNoteList);
 };
+
 
 $saveNoteBtn.on("click", handleNoteSave);
 $noteList.on("click", ".list-group-item", handleNoteView);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,40 @@
-var savedNotes = require("../db/db.json");
+let savedNote = require("../db/db");
 const fs = require('fs');
 
 
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
-    res.json(savedNotes);
+    res.json(savedNote);
   });
 
 
 
   app.post("/api/notes", function(req, res) {
-    savedNotes.push(req.body);
-    res.json("saved");
+    let newNote = req.body;
+  
+      savedNote.push(newNote);
+        
+    fs.writeFile("./db/db.json", JSON.stringify(savedNote),(err) => {
+      if (err) {
+        console.log(err); 
+        res.sendStatus(404);
+      } 
+      else { 
+          console.log("Success!");
+          res.sendStatus(200);
+      }
+    });  
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-    var id = req.params.id;
-    var atID = (element) => element.id === id;
-    var deleteNote = savedNotes.findIndex(atID);
+    let noteid = req.params.id;
+    let newID = 0;
 
-    savedNotes.splice(deleteNote, 1);
-        fs.writeFile("./db/db.json", JSON.stringify(savedNotes),(err) => {
+    let deleteNote = savedNote.findIndex(atID);
+
+    savedNote.splice(deleteNote, 1);
+        fs.writeFile("./db/db.json", JSON.stringify(savedNote),(err) => {
       if (err) {
         console.log(err); 
         res.sendStatus(404);
